1355802b7a35ba4ae96f274688af87e5f13bfb0a
Moses Simanungkalit <mosesgilbert4@gmail.com>
2025-05-06 03:45:47 +0700

    test: update-testing

--- src/validators/productSchema.js
+++ src/validators/productSchema.js
@@ -23,7 +23,25 @@ const productSchema = z
 
 const productUpdateSchema = z
   .object({
+    name: z
+      .string()
+      .min(1, "Name is required")
+      .refine((val) => val !== null, { message: "Name is required" })
+      .optional(),
+
     stock: z.number().nonnegative("Stock cannot be negative").optional(),
+
+    unit: z
+      .string()
+      .min(1, "Unit is required")
+      .refine((val) => val !== null, { message: "Unit is required" })
+      .optional(),
+
+    category: z
+      .string()
+      .min(1, "Category is required")
+      .refine((val) => val !== null, { message: "Category is required" })
+      .optional(),
   })
   .passthrough();
 
