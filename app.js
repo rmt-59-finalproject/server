1696ea29179d07049af9656fc5d845706a824aba
hafidzahm <hafizhahm123@gmail.com>
2025-06-10 16:51:02 +0700

    fix: fix CORS issue

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const CLIENT = process.env.CLIENT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ credentials: true, origin: CLIENT }));
 app.use(cookieParser());
 
 app.use("/api", require("./src/routes/index"));
