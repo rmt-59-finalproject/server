7bc1eb5ecef2e9943bda6d574bca607af765f29c
hafidzahm <hafizhahm123@gmail.com>
2025-06-10 16:58:57 +0700

    fix: CORS issue: Access to XMLHttpRequest at 'https://stockify-api.hafizh.web.id/api/login' from origin 'https://stockify-project.web.app' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const CLIENT = process.env.CLIENT;
+// Define CORS options
+const corsOptions = {
+  origin: CLIENT,
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", // Allow common methods
+  allowedHeaders: "Content-Type,Authorization,X-Requested-With", // Allow common headers
+};
+
+// Handle preflight requests for all routes
+app.options("*", cors(corsOptions));
+
+// Enable CORS for all other requests
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: CLIENT }));
 app.use(cookieParser());
 
 app.use("/api", require("./src/routes/index"));
