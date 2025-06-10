6d96eb3b94b5d97985675511465009a9675588bb
hafidzahm <hafizhahm123@gmail.com>
2025-06-10 16:45:05 +0700

    refactoring

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,14 @@
-const UserController = require('../controllers/user.controller');
-const { authentication, authWarehouse } = require('../middlewares/auth.middleware');
-const user = require('express').Router();
+const UserController = require("../controllers/user.controller");
+const {
+  authentication,
+  authWarehouse,
+} = require("../middlewares/auth.middleware");
+const user = require("express").Router();
 
-user.post('/register', authentication, authWarehouse, UserController.register);
-user.post('/login', UserController.login);
-user.get('/login', authentication, UserController.checkToken);
-user.get('/logout', authentication, UserController.logout);
-user.get('/users', authentication, authWarehouse, UserController.readAllUsers);
+user.post("/register", authentication, authWarehouse, UserController.register);
+user.post("/login", UserController.login);
+user.get("/login", authentication, UserController.checkToken);
+user.get("/logout", authentication, UserController.logout);
+user.get("/users", authentication, authWarehouse, UserController.readAllUsers);
 
 module.exports = user;
